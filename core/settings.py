2fbcb6b3f826bcc1efcaf24632ffdd4d02afedbe
Turkanyasar <turkanaliyevayasar@gmail.com>
2026-05-30 16:54:19 +0000

    cors

--- core/settings.py
+++ core/settings.py
@@ -30,9 +30,12 @@ INSTALLED_APPS = [
     # "django.contrib.staticfiles",
     "rest_framework",
     "api",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -42,6 +45,10 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
+
+CORS_ALLOWED_ORIGINS = [
+    "https://turkanyasar.github.io"
+]
 ROOT_URLCONF = "core.urls"
 
 TEMPLATES = [
